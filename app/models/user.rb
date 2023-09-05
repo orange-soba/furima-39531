3f9742cdaab8ccfa003e44f4d0f142333daf0e8e
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-05 16:19:58 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は、全角（漢字・ひらがな・カタカナ）での入力が必須です" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は、全角（漢字・ひらがな・カタカナ）での入力が必須です" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "は、全角（カタカナ）での入力が必須です" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "は、全角（カタカナ）での入力が必須です" }
   validates :birthday, presence: true
 end
