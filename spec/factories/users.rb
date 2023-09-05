0f628fffd3f44d673fe31a6b2d7c704d689df793
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-05 21:34:32 +0900

    ダミーデータ作成のための設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password}
+    password_confirmation {password}
+    last_name             {Faker::Name.last_name}
+    first_name            {Faker::Name.first_name}
+    last_name_kana        {Gimei.name.last.katakana}
+    first_name_kana       {Gimei.name.first.katakana}
+    birthday              {Faker::Date.birthday(min_age: 5, max_age: 92)}
   end
 end
