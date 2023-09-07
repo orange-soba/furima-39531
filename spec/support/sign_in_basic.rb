64516c6f2ffc1f69d282437e31e1ea62a0dba556
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-07 20:01:53 +0900

    必要のない結合テストの削除

--- spec/support/sign_in_basic.rb
+++
@@ -1,8 +0,0 @@
-module SignInBasic
-  def sign_in_basic(path)
-    user = ENV["BASIC_AUTH_USER"]
-    password = ENV["BASIC_AUTH_PASSWORD"]
-
-    visit "http://#{user}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
-  end
-end
