29f01f3842385046e81e90783f3eecbe6a589913
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-07 15:34:37 +0900

    結合テストの実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Users", type: :system do
   end
 
   it 'ログアウト状態の時はトップページのヘッダーに「新規登録」「ログイン」ボタンが表示される' do
-    sign_in_basic(root_path)
+    sign_in_basic(root_path) # Basic認証を通過してトップページに遷移
     expect(page).to have_content('新規登録')
     expect(page).to have_content('ログイン')
   end
@@ -23,9 +23,15 @@ RSpec.describe "Users", type: :system do
     expect(current_path).to eq(new_user_registration_path)
   end
   it 'ログイン状態の時はトップページのヘッダーに「ユーザーのニックネーム」と「ログアウト」ボタンが表示される' do
-    log_in(@user)
-    binding.pry
+    log_in(@user) # Basic認証を通過してトップページに遷移した後、ログインしてトップページに遷移
+    expect(page).to have_content(@user.nickname)
+    expect(page).to have_content("ログアウト")
   end
   it 'トップページの「ログアウト」ボタンをクリックするとログアウトする' do
+    log_in(@user)
+    expect(page).to have_content("ログアウト")
+    click_on "ログアウト"
+    sleep 1
+    expect(page).to have_content("ログイン") # 一番上のテストよりログアウト状態の確認
   end
 end
