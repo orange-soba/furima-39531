8e48dba8a939027bce9d57cdf2dca59192563939
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-08 19:00:51 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,8 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
   belongs_to :user
+
+  validates :name, :explanation, presence: true
+  validates :category_id, :condition_id, :fee_id, :prefecture_id, :shipping_day_id, presence: true, numericality: { other_than: 0 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
 end
