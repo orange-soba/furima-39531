69b53d5ff991fc96c86cf09fcb55e4383a9817db
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-08 21:11:58 +0900

    itemの保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,15 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def move_to_sign_in
@@ -15,5 +24,10 @@ class ItemsController < ApplicationController
       redirect_to new_user_session_path
     end
   end
-  
+
+  def item_params
+    permit_arr = [:name, :explanation, :category_id, :condition_id, :fee_id, :prefecture_id, :shipping_day_id, :price]
+    params.require(:item).permit(permit_arr).merge(user_id: current_user.id)
+  end
+
 end
