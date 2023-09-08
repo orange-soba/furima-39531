7399f581f7fa60ecc9e8d30b0a503336fcc5bddf
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-08 20:26:39 +0900

    ログイン/アウト状態による出品ボタンを押した時の分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sign_in, only: [:new]
 
   def index
   end
@@ -7,4 +8,12 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  private
+
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+  
 end
