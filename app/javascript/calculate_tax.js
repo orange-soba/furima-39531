7a1cfca7939e3c18c1ed4a1e2c659409efabdf5c
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-09 14:44:38 +0900

    価格の入力によりイベント発火

--- app/javascript/calculate_tax.js
+++ app/javascript/calculate_tax.js
@@ -1,5 +1,9 @@
 function calculate_tax(){
   // console.log('発火');
+  const priceInput = document.getElementById("item-price")
+  priceInput.addEventListener('keyup', () => {
+    // console.log(priceInput.value);
+  });
 };
 
 window.addEventListener('turbo:load', calculate_tax);
