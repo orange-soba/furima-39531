f9d908126d85de48626f56ca62360209c65d77dc
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-09 17:23:39 +0900

    単体テストのための準備

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  
+  describe "商品出品機能" do
+    context "商品を出品できる場合" do
+    end
+    context "商品を出品できない場合" do
+    end
+  end
 end
