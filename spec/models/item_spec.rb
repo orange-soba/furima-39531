3fbd672df1c340ebfabeff3f7cc59d8f345dbbd3
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-09 18:37:01 +0900

    単体テストの失敗例

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,6 +12,71 @@ RSpec.describe Item, type: :model do
       end
     end
     context "商品を出品できない場合" do
+      it "imageが空だと出品できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it "nameが空だと出品できない" do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "explanationが空だと出品できない" do
+        @item.explanation = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it "category_idが0だと出品できない" do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it "condition_idが0だと出品できない" do
+        @item.condition_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it "fee_idが0だと出品できない" do
+        @item.fee_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Fee can't be blank")
+      end
+      it "prefecture_idが0だと出品できない" do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it "shipping_day_idが0だと出品できない" do
+        @item.shipping_day_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it "priceが空だと出品できない" do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "priceが299以下だと出品できない" do
+        @item.price = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than 299")
+      end
+      it "priceが10000000以上だと出品できない" do
+        @item.price = 99999999
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
+      end
+      it "priceが半角の数字でないと出品できない" do
+        @item.price = "９９９９９"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "userと紐づいていないと出品できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
     end
   end
 end
