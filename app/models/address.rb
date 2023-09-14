3e8fe1ac47c6a6c8197bbd0270abdb02a5786124
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-15 01:40:54 +0900

    一時的なバリテーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :order
+
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  with_options presence: true do
+    validates :postal_code,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :city, :house_number, :phone_number
+  end
 end
