830d494b63484c568de042082552ce19c5ac0e2b
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-15 02:22:38 +0900

    Formオブジェクトの作成とバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,9 +3,9 @@ class Address < ApplicationRecord
   belongs_to :prefecture
   belongs_to :order
 
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-  with_options presence: true do
-    validates :postal_code,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :city, :house_number, :phone_number
-  end
+  # validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  # with_options presence: true do
+    # validates :post_code,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    # validates :city, :house_number, :phone_number
+  # end
 end
