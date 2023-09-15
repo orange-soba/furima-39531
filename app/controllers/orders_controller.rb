7df19bb745100fad649fc0a7357f4e0916926d81
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-15 16:19:25 +0900

    tokenの受け取り

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
+      @item = Item.find(params[:item_id])
       render :index, status: :unprocessable_entity
     end
   end
@@ -18,7 +19,8 @@ class OrdersController < ApplicationController
 
   def order_params
     item_id = params[:item_id]
+    token = params[:token]
     params.require(:order_address).permit(:post_code, :prefecture_id, :city, :house_number, :building, :phone_number)
-          .merge(user_id: current_user.id, item_id: item_id)
+          .merge(user_id: current_user.id, item_id: item_id, token: token)
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :house_number, :building, :phone_number
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :house_number, :building, :phone_number, :token
 
   with_options presence: true do
     validates :user_id, :item_id, :city, :house_number, :phone_number
