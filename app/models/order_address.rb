f39cf51e729ce431e7ff01163a50b1853e96250f
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-15 17:32:45 +0900

    エラーメッセージの表示

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,8 +3,8 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :house_number, :building, :phone_number, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :house_number, :phone_number
     validates :post_code,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :user_id, :item_id, :city, :house_number, :phone_number
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
