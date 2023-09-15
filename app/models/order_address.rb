a0407bbbace0c37a43564e2507ab1195e86e4db3
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-15 17:54:53 +0900

    バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,8 +3,9 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :house_number, :building, :phone_number, :token
 
   with_options presence: true do
-    validates :post_code,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :user_id, :item_id, :city, :house_number, :phone_number, :token
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :user_id, :item_id, :city, :house_number, :token
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Only 10 to 11 digits in half-width numbers." }
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
