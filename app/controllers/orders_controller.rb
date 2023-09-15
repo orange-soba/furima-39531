cef881817ba5c4457c0ab5b322d29111baa5f7c6
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-15 17:28:22 +0900

    決済処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,17 @@
 class OrdersController < ApplicationController
+  before_action :set_item
+
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_order
       @order_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index, status: :unprocessable_entity
     end
   end
@@ -22,5 +23,18 @@ class OrdersController < ApplicationController
     token = params[:token]
     params.require(:order_address).permit(:post_code, :prefecture_id, :city, :house_number, :building, :phone_number)
           .merge(user_id: current_user.id, item_id: item_id, token: token)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_order
+    Payjp.api_key = 'sk_test_239743be9188d7125b6578f5'
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy',
+    )
   end
 end
