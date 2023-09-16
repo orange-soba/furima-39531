11c5d4e6f5eda562ca3d9b8fa0c88f282196c302
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-16 20:18:37 +0900

    アソシエーションの追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,10 +2,4 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :order
-
-  # validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-  # with_options presence: true do
-    # validates :post_code,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    # validates :city, :house_number, :phone_number
-  # end
 end
