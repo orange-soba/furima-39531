f85869e0c7310c825cb57117964f05cd4d9d433b
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-16 16:25:38 +0900

    ダミーデータの作成

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_address do
+    post_code     { Faker::Number.number(digits: 3).to_s.insert(3, '-') }
+    prefecture_id { Faker::Number.between(from: 1, to: 47) }
+    city          { Gimei.address.kanji }
+    house_number  { Faker::Number.number(digits: 3) }
+    building      { Faker::Games::Zelda.location }
+    phone_number  { Faker::Number.number(digits: 10) }
+    token         { "tok_abcdefghijk00000000000000000" }
+  end
+end
