520aeac6c00e705e8d5e61bf5fe9f0e01d2f3940
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-27 15:21:03 +0900

    不要なメソッドの削除

--- app/javascript/own_alert.js
+++
@@ -1,9 +0,0 @@
-function own_alert() {
-  const flashCheck = document.getElementById('flash-check');
-  if (flashCheck) {
-    const alertSentence = flashCheck.innerHTML;
-    alert(alertSentence);
-  };
-};
-
-window.addEventListener('turbo:load', own_alert);
