bb0d2ed362133e5946af84d7503aab84fd28c0f0
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-28 14:19:05 +0900

    お気に入り済か判断するメソッド

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,4 +22,8 @@ class Item < ApplicationRecord
   def self.ransackable_associations(auth_object = nil)
     ["user"]
   end
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
