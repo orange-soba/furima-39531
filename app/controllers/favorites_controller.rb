30d3bad896325d364b8cc0b8b65a04e1966779a1
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-28 16:25:02 +0900

    非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,22 @@
 class FavoritesController < ApplicationController
+  before_action :set_item
+
   def create
     favorite = current_user.favorites.create(item_id: params[:item_id])
-    redirect_back(fallback_location: request.referer)
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
     favorite = Favorite.find_by(item_id: params[:item_id], user_id: current_user.id)&.destroy
-    redirect_back(fallback_location: request.referer)
+    respond_to do |format|
+      binding.pry
+      format.js
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
