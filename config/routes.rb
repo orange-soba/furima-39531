6ab8b8ea37bbd0d48b36027ca9454a29b0fec634
orange-soba <t.sato.terusa6k@gmail.com>
2023-09-28 14:11:54 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
+    resource :favorites, only: [:create, :destroy]
     resources :orders, only: [:index, :create]
     collection do
       get 'search'
