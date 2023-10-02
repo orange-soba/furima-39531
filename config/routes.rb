3073ddb47d79008cd9a38e008bfb89ceff7161b5
orange-soba <t.sato.terusa6k@gmail.com>
2023-10-02 13:21:15 +0900

    editの省略

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items do
     resource :favorites, only: [:create, :destroy]
-    resources :rooms, only: [:index, :edit, :update]
+    resources :rooms, only: [:index, :update]
     resources :orders, only: [:index, :create]
     collection do
       get 'search'
