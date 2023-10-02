dd065d0e437dd6815a3ada10f1f6b9a5c6894ed8
orange-soba <t.sato.terusa6k@gmail.com>
2023-10-03 00:50:10 +0900

    メッセージの投稿

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,25 @@
 class MessagesController < ApplicationController
+  before_action :set_data
+
+  def create
+    @message = Message.new(message_params)
+    if @message.valid?
+      @message.save
+      redirect_to item_rooms_path(@item.id)
+    else
+      @messages = Message.where(room_id: @room.id)
+      render 'rooms/index', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_data
+    @item = Item.find(params[:item_id])
+    @room = @item.room
+  end
+
+  def message_params
+    params.require(:message).permit(:message).merge(room_id: @room.id, user_id: current_user.id)
+  end
 end
