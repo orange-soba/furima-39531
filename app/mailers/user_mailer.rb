a75041ddddd0b62509806782c902520edf604ba4
orange-soba <t.sato.terusa6k@gmail.com>
2023-10-04 17:35:50 +0900

    メールアドレスの環境変数化

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
-  default from: email_address_with_name('furima.39531@gmail.com', 'Furima-39531')
-  
+  default from: email_address_with_name(ENV['GOOGLE_EMAIL'], 'Furima-39531')
+
   def test_mail
     @user = params[:user]
     mail(
