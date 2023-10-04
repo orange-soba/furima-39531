a32e6f1aa0e057424a9e2b007c902e11f854bb7a
orange-soba <t.sato.terusa6k@gmail.com>
2023-10-04 17:16:21 +0900

    コントローラーにてメーラーの呼び出し

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,8 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     if @message.valid?
       @message.save
+      user_mail = User.find(4) 
+      UserMailer.with(user: user_mail).test_mail.deliver_later
       redirect_to item_rooms_path(@item.id)
     else
       @messages = Message.where(room_id: @room.id)
