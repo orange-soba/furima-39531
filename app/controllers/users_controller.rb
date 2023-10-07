f07d6dc9c778a3c6421c0b138827b15d97e2b73b
orange-soba <t.sato.terusa6k@gmail.com>
2023-10-07 16:55:05 +0900

    ユーザー詳細ページでのお気に入り登録商品の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @favorite_items = @user.favorites.map(&:item)
     @items = @user.items.order('created_at DESC')
     @graph = BuildGraph.new(@items).build_graph
     # BuilGraphは app/services にあります
